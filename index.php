b6a4235894da1e526fe6f58d336df8bb618fb367
Abraham <abrahamfernandez07201995@gmail.com>
2023-06-13 22:57:28 +0800

    Added Database and connect to mysqli

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
- 
+<?php 
+
+// Connect to database
+
+$conn = mysqli_connect('localhost', 'abez27', 'password', 'ninja_pizza');
+
+// Check connection
+
+if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
+?> 
 
 <?php include('template_part/header.php'); ?>
 
