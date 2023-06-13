df660995fefbe4c5691b4f89a4e59fe7b4091568
Abraham <abrahamfernandez07201995@gmail.com>
2023-06-13 23:23:58 +0800

    Explode , make the ingredient as block

--- index.php
+++ index.php
@@ -32,6 +32,9 @@ mysqli_close($conn);
 // print_r($pizzas);
 
 
+// print_r(explode(',', $pizzas[0]['ingredients']));
+
+
 ?> 
 
 <?php include('template_part/header.php'); ?>
@@ -45,7 +48,11 @@ mysqli_close($conn);
             <div class="card z-depth-0">
                 <div class="card-content center">
                     <h6><?php echo htmlspecialchars($pizza['title']);?></h6>
-                    <div><?php echo htmlspecialchars($pizza['ingredients']);?></div>
+                    <ul>
+                        <?php foreach(explode(',', $pizza['ingredients'])as $ing){?>
+                            <li><?php echo htmlspecialchars($ing) ;?></li>
+                        <?php }?>
+                    </ul>
                 </div>
                 <div class="card-action right-align">
                     <a href="" class="brand-text">More Info</a>
